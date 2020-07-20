2f365f72b91437290bc64f40f0c4241163f2d558
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-20 18:13:26 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 def run_guessing_game
   computer_number = rand(6) + 1
-  #puts "Please enter a number between 1 and 6"
+  puts "Please enter a number between 1 and 6"
   user_input = gets.chomp
   if user_input == computer_number.to_s
     puts "You guessed the correct number!"
